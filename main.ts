8203e88cc3174f5b9f6246f655f221f9e44f6285
manucrack-star <110508683+manucrack-star@users.noreply.github.com>
2022-08-04 21:01:35 -0500

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,166 @@
 let RANDOM_ACTIVITI = 0
 input.onButtonPressed(Button.A, function () {
-    RANDOM_ACTIVITI = randint(0, 4)
+    RANDOM_ACTIVITI = randint(0, 6)
     if (RANDOM_ACTIVITI == 0) {
-        basic.showString("JUGAR")
+        basic.showString("Jugar Videojuegos")
+        for (let index = 0; index < 4; index++) {
+            basic.showLeds(`
+                . . . . .
+                # # # # #
+                # . # . #
+                # # # # #
+                # # . # #
+                `)
+            basic.showLeds(`
+                # # # # #
+                # . # . #
+                # # # # #
+                # # . # #
+                . . . . .
+                `)
+        }
     } else if (RANDOM_ACTIVITI == 1) {
-        basic.showString("ESTUDIAR")
+        basic.showString("Estudiar ")
+        for (let index = 0; index < 4; index++) {
+            basic.showLeds(`
+                # # # # #
+                # . # . #
+                # . # . #
+                # . # . #
+                # # # # #
+                `)
+            basic.showLeds(`
+                . # # # .
+                . # # # .
+                . # # # .
+                . # # # .
+                . # # # .
+                `)
+        }
     } else if (RANDOM_ACTIVITI == 2) {
-        basic.showString("BAILAR")
+        basic.showString("Hacer Tareas")
+        for (let index = 0; index < 4; index++) {
+            basic.showLeds(`
+                . . . # #
+                . . # # #
+                . . # # .
+                . . . . .
+                . . . . .
+                `)
+            basic.showLeds(`
+                . . . # #
+                . . # # #
+                . # # # .
+                . # # . .
+                . . . . .
+                `)
+            basic.showLeds(`
+                . . . # #
+                . . # # #
+                . # # # .
+                # # # . .
+                # # . . .
+                `)
+        }
     } else if (RANDOM_ACTIVITI == 3) {
-        basic.showString("CANTAR")
+        basic.showString("Sacar los perros")
+        for (let index = 0; index < 4; index++) {
+            basic.showLeds(`
+                . # . # .
+                . # # # .
+                # . # . #
+                . # # # .
+                . . # . .
+                `)
+            basic.showLeds(`
+                . # . # .
+                . # # # .
+                # # # # #
+                . # # # .
+                . . # . .
+                `)
+        }
     } else if (RANDOM_ACTIVITI == 4) {
-        basic.showString("MIRAR TV")
-    } else {
-    	
+        basic.showString("Mirar Television")
+        for (let index = 0; index < 4; index++) {
+            basic.showLeds(`
+                # # # # #
+                # . . . #
+                # # # # #
+                . . # . .
+                . # # # .
+                `)
+            basic.showLeds(`
+                # # # # #
+                # # # # #
+                # # # # #
+                . . # . .
+                . # # # .
+                `)
+        }
+    } else if (RANDOM_ACTIVITI == 5) {
+        basic.showString("Salir a caminar")
+        for (let index = 0; index < 4; index++) {
+            basic.showLeds(`
+                . . . . .
+                # # # . .
+                . . # # #
+                . . . . #
+                # # # # #
+                `)
+            basic.showLeds(`
+                # # # . .
+                . . # # #
+                . . . . #
+                # # # # #
+                . . . . .
+                `)
+            basic.showLeds(`
+                . # # # .
+                . # . . #
+                . # . . .
+                . # # # #
+                . . . . .
+                `)
+            basic.showLeds(`
+                . . . . .
+                . . # # #
+                . . # . .
+                . . # . .
+                . . # # #
+                `)
+        }
+    } else if (RANDOM_ACTIVITI == 6) {
+        basic.showString("Nadar")
+        for (let index = 0; index < 4; index++) {
+            basic.showLeds(`
+                . . # . .
+                . # # # .
+                # # # # #
+                . # # # .
+                . . . . .
+                `)
+            basic.showLeds(`
+                . . . . .
+                . . # . .
+                . # # # .
+                # # # # #
+                . # # # .
+                `)
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . # . .
+                . # # # .
+                # # # # #
+                `)
+            basic.showLeds(`
+                . . . # .
+                # . . . .
+                . . . . #
+                . # . # .
+                # # # # #
+                `)
+        }
     }
 })
